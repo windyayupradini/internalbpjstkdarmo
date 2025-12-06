dd7697f6ee35cb376f3ea3eb364e76d9dd1125f0
windyayupradini <149036199+windyayupradini@users.noreply.github.com>
2025-12-06 18:41:33 +0700

    Delete api directory

--- api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: application/json");
-
-echo json_encode([
-    "status" => "success",
-    "message" => "PHP Native berjalan di Vercel!",
-    "time" => date("Y-m-d H:i:s")
-]);
