46d9f6519d3be1109c8dbf893a5bfa7d5359735c
windyayupradini <149036199+windyayupradini@users.noreply.github.com>
2025-12-08 16:48:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 //     ['title'=>'SMART','url'=>'https://smart.bpjsketenagakerjaan.go.id/#/Login'],
 //     ['title'=>'SIPATUH','url'=>'https://sipatuh.bpjsketenagakerjaan.go.id/'],
 //     ['title'=>'E-LEARNING','url'=>'https://elearning.bpjsketenagakerjaan.go.id/login/index.php'],
+//     ['title'=>'MONITORING','url'=>'https://sites.google.com/bpjsketenagakerjaan.go.id/monitoringusdmn13/beranda'],
 //   ],
 //   'KEPESERTAAN' => [
 //     ['title'=>'HCIS','url'=>'https://hcis.bpjsketenagakerjaan.go.id/hcis/sunfish5/ehrm/default.cfm'],
